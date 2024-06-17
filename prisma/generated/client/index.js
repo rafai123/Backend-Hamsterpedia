8669c487f071f070e0133160607274a08df57fc3
Muhammad Rafai <rafaimhd123@gmail.com>
2024-06-18 00:18:41 +0700

    change schema.prisma

--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -148,7 +148,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": "../../../.env",
+    "rootEnvPath": null,
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
