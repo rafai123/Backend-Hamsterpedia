11471c79518f5c5408e306f559ac6cf0834a795a
Muhammad Rafai <rafaimhd123@gmail.com>
2024-06-18 20:31:25 +0700

    move prisma from utils to index

--- index.js
+++ index.js
@@ -10,7 +10,19 @@ const port = 3003;
 
 // import from utils
 const S3 = require("./utils/S3");
-const prisma = require("./utils/prisma");
+// const prisma = require("./utils/prisma");
+
+const { PrismaClient } = require('@prisma/client')
+const { PrismaLibSQL } = require('@prisma/adapter-libsql')
+const { createClient } = require('@libsql/client')
+
+const libsql = createClient({
+    url: `${process.env.TURSO_DATABASE_URL}`,
+    authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+})
+
+const adapter = new PrismaLibSQL(libsql)
+const prisma = new PrismaClient({ adapter })
 
 // middleware
 app.use(cors());
