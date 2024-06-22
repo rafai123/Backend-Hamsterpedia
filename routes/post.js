88dc23059d15e941534ea678c17c560b2ced1318
Muhammad Rafai <rafaimhd123@gmail.com>
2024-06-23 00:19:49 +0700

    fix category for allposts

--- routes/post.js
+++ routes/post.js
@@ -55,7 +55,7 @@ router.post("/addpost", upload.single("file"), async (req, res) => {
 
 router.get("/allposts", async (req, res) => {
     const category = req.query.category
-    (!category ? category = "" : category = category)
+    (!category ? category = "general" : category = category)
     console.log(category)
     try {
         const allPosts = await prisma.posts.findMany({
